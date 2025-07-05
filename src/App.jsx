ea5e30cea6bb71af202bd8b594ecf2a67ec66bec
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-07-05 13:17:17 +0300

    feat: integrate Font Awesome icons in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,17 +53,17 @@ function App() {
           <div className="about-text">
             <h2 className="section-title">À propos de nous</h2>
             <div className="about-item">
-              <span className="about-icon">Icon 1</span>
+              <span className="about-icon"><i className="fas fa-graduation-cap"></i></span>
               <h3 className="about-subtitle">+3ans</h3>
               <p className="section-text">Depuis 3 ans, nous formons ceux qui décodent le futur, aux métiers de l’informatique. Nous sommes un groupe d’experts, de professionnels et des passionnés de l’informatique de Madagascar et du monde entier.</p>
             </div>
             <div className="about-item">
-              <span className="about-icon">Icon 2</span>
+              <span className="about-icon"><i className="fas fa-users"></i></span>
               <h3 className="about-subtitle">+250 étudiants</h3>
               <p className="section-text">Plus de 250 étudiants et plus d’une dizaine d’entreprises ont déjà fait confiance à notre formation habilitée par l’État malgache. Avec eux tous, nous sommes fiers de pouvoir parler de la “grande famille HEI”.</p>
             </div>
             <div className="about-item">
-              <span className="about-icon">Icon 3</span>
+              <span className="about-icon"><i className="fas fa-star"></i></span>
               <h3 className="about-subtitle">Notre mission</h3>
               <p className="section-text">Notre mission est d’amener des jeunes malgaches sur des domaines porteurs comme l’intelligence artificielle, la cybersécurité ou la programmation afin de favoriser leur employabilité et le développement de Madagascar.</p>
             </div>
